a7803e6d3aa1dff8d0374eb989b7a3d8e03fa55e
guojunhao <guojunhao.@zzschj.com>
2022-07-22 10:47:53 +0800

    table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <Home></Home> -->
+    <MyTable
+      :columns="this.columns"
+      :table="this.table"
+      :conditions="this.conditions"
+      :columnsName="this.columnsName"
+      v-slot="value"
+    >
+      <el-button size="small" @click.native="change(value.index)"
+        >编辑</el-button
+      >
+      <el-button size="small" type="danger" @click.native="del(value.index)"
+        >删除</el-button
+      >
+      <el-button size="small" @click.native="add()">添加</el-button>
+    </MyTable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from "./pages/Home.vue";
+import MyTable from "./components/MyTable.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Home, MyTable },
+  data() {
+    return {
+      columns: {
+        id: "id",
+        account: "account",
+        password: "password",
+        college: "college",
+        // category1_id: "category1_id",
+        // category2_id: "category2_id",
+        // category3_id: "category3_id",
+        // name: "name",
+        // trademark: "trademark",
+        // key_words: "key_words",
+        // price: "price",
+      },
+      conditions: {
+        // id: "1",
+      },
+      columnsName: {
+        id: "主键",
+        account: "账号",
+        password: "密码",
+        college: "学院",
+        // category1_id: "分类1",
+        // category2_id: "分类2",
+        // category3_id: "分类3",
+        // name: "商品名称",
+        // trademark: "品牌",
+        // key_words: "关键字",
+        // price: "价格",
+      },
+      table: "user",
+    };
+  },
+  methods: {
+    change(index) {
+      this.$bus.$emit("edit", index);
+    },
+    del(index) {
+      this.$bus.$emit("del", index);
+    },
+    add() {
+      this.$bus.$emit("add");
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,82 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import vuex from "vuex";
+import VueRouter from "vue-router";
+import router from "./router/index.js";
+import store from "./sotre/index"
+import "./style/global.css";
+
+import MyTable from "./components/MyTable.vue"
+
+Vue.use(vuex);
+Vue.use(VueRouter);
 Vue.config.productionTip = false
+
+Vue.component(MyTable.name, MyTable);
+
+import {
+  Button,
+  Input,
+  Menu,
+  MenuItem,
+  Submenu,
+  Container,
+  Header,
+  Main,
+  Pagination,
+  MenuItemGroup,
+  Aside,
+  Table,
+  TableColumn,
+  Dialog,
+  Form,
+  FormItem,
+  Select,
+  Option,
+  Checkbox,
+  CheckboxGroup,
+  CheckboxButton,
+  Row,
+  Col,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+} from "element-ui";
+
+Vue.use(Button);
+Vue.use(Input);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Main);
+Vue.use(Pagination);
+Vue.use(MenuItemGroup);
+Vue.use(Aside);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Dialog);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+Vue.use(CheckboxButton);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
 
 new Vue({
   render: h => h(App),
+  store,
+  // 
+  router: router,
+  beforeCreate() {
+    Vue.prototype.$bus = this
+  },
 }).$mount('#app')
